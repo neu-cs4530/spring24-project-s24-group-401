a9229e7e6d5132797650e78eeb121e7ac6277a2b
adichak27 <adichak2002@gmail.com>
2024-03-11 16:58:38 -0400

    finished _join method

--- townService/src/town/games/HangmanGame.ts
+++ townService/src/town/games/HangmanGame.ts
@@ -1,3 +1,4 @@
+import InvalidParametersError, { GAME_FULL_MESSAGE, PLAYER_ALREADY_IN_GAME_MESSAGE } from '../../lib/InvalidParametersError';
 import Player from '../../lib/Player';
 import { GameMove, HangmanGameState, HangmanMove } from '../../types/CoveyTownSocket';
 import Game from './Game';
@@ -10,6 +11,20 @@ import Game from './Game';
 
 
 export default class HangmanGame extends Game<HangmanGameState, HangmanMove> {
+    // a list of playerIDs of all Players currently in the game 
+    private allPlayers: Array<String>;
+    private maxPlayersAllowed = 10;
+    public constructor(targetWord: String) {
+        // word to be guessed
+        super({
+            status: 'WAITING_TO_START',
+            word: 'testWord',
+            guessedLetters: [],
+            maxIncorrectGuesses: 6,
+            incorrectGuesses: 0,
+        });
+        this.allPlayers = [];
+    }
     protected _leave(player: Player): void {
         throw new Error('Method not implemented.');
     }
@@ -18,7 +33,25 @@ export default class HangmanGame extends Game<HangmanGameState, HangmanMove> {
         throw new Error('Method not implemented.');
     }
     
+
+    /**
+     * Joins a player to the game.
+     *
+     * @throws InvalidParametersError if the player is already in the game (PLAYER_ALREADY_IN_GAME_MESSAGE)
+     * @throws InvalidParametersError if the game is full (GAME_FULL_MESSAGE)
+     *
+     * @param player the player to join the game
+     */
     protected _join(player: Player) {
-        throw new Error('Method not implemented.');
+        if (this.allPlayers.length === this.maxPlayersAllowed) {
+            throw new InvalidParametersError(GAME_FULL_MESSAGE);
+        }
+        if (this.allPlayers.includes(player.id)) {
+            throw new InvalidParametersError(PLAYER_ALREADY_IN_GAME_MESSAGE);
+        }
+        if (this.allPlayers.length === 0) {
+            this.allPlayers.push(player.id);
+            return;
+        }
     }
 }
--- townService/src/town/games/HangmanGameArea.ts
+++ townService/src/town/games/HangmanGameArea.ts
@@ -2,9 +2,6 @@ import { InteractableCommand, InteractableCommandReturnType, InteractableType, P
 import GameArea from "./GameArea";
 import HangmanGame from "./HangmanGame";
 
-
-
-
 export default class HangmanGameArea extends GameArea<HangmanGame> {
     protected getType(): InteractableType {
         throw new Error("Method not implemented.");
