bb87e0a6e9a8362dba6ef2a417676c5527e2e49a
adichak27 <adichak2002@gmail.com>
2024-03-11 16:10:59 -0400

    Create basic  back-end infrastucture for Hangman including types

---
+++ townService/src/town/games/HangmanGameArea.ts
@@ -0,0 +1,15 @@
+import { InteractableCommand, InteractableCommandReturnType, InteractableType, Player } from "../../types/CoveyTownSocket";
+import GameArea from "./GameArea";
+import HangmanGame from "./HangmanGame";
+
+
+
+
+export default class HangmanGameArea extends GameArea<HangmanGame> {
+    protected getType(): InteractableType {
+        throw new Error("Method not implemented.");
+    }
+    public handleCommand<CommandType extends InteractableCommand>(command: CommandType, player: Player): InteractableCommandReturnType<CommandType> {
+        throw new Error("Method not implemented.");
+    }
+}
