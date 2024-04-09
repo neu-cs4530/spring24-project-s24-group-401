575568d990d84f81702ee499a5c9d82773ee0a60
Ronit Setia <48104496+Ronit02@users.noreply.github.com>
2024-04-09 11:51:40 -0400

    Commenting out database connection
    Due to Heroku paywall we cannot deploy database, commenting out to we can deploy and run.

--- townService/src/lib/database.ts
+++ townService/src/lib/database.ts
@@ -1,9 +1,9 @@
-import { Sequelize } from 'sequelize';
+// import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('template1', 'postgres', '', {
-  host: 'localhost',
-  port: 5432,
-  dialect: 'postgres',
-});
+// const sequelize = new Sequelize('template1', 'postgres', '', {
+//   host: 'localhost',
+//   port: 5432,
+//   dialect: 'postgres',
+// });
 
-export default sequelize;
+// export default sequelize;
